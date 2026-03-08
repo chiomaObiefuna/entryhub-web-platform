c2599d066e3dd9b4208f61dee61725322ab3c216
Chioma Obiefuna <chioma.obiefuna7@gmail.com>
2026-03-08 17:40:45 +0100

    initial setup with folder structure

---
+++ src/styles/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+  primary: '#0B2B1E',
+  button: '#FF8C00',
+  span: '#FF8C00',
+  text: '#FFFFFF',
+  h3: '#092419',
+  border: '2px solid #FF9F2B',
+  border: '2px solid #092419',
+  background: '#CED5D2',
+  background: '#D9D9D9',
+  button: '#071D14',
+  background: '#5C7269',
+  
+
+
+
+
+}
