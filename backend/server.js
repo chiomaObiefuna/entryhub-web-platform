8dc4e274dbc7144acca0d593b0f58ff0568a52a6
Donchess1 <stepabod@yahoo.com>
2026-03-17 10:41:18 +0100

    add swagger.json

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to DB
 connectDB();
 
 // Routes
@@ -36,7 +35,7 @@ const options = {
     },
     servers: [
       { url: "http://localhost:5000" },
-      { url: "https://your-app-name.onrender.com" } // 👈 replace this
+      { url: "https://eventhub-backend-pxoz.onrender.com" }
     ]
   },
   apis: ["./routes/*.js"]
@@ -47,7 +46,6 @@ const specs = swaggerJsdoc(options);
 // Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-// ✅ THIS is what Postman needs
 app.get("/api-docs.json", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(specs);
