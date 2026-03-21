c50a49860ced40a9d19ae7d66d07ca124387d55a
Emmanuel O.O. <emmaigbagbo37@gmail.com>
2026-03-21 11:56:50 +0100

    modified:   src/App.jsx renamed:    src/components/EventDetails/EventDetails.css -> src/pages/EventDetails/EventDetails.css renamed:    src/components/EventDetails/EventDetails.jsx -> src/pages/EventDetails/EventDetails.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Home-page/Navbar/Navbar'
 import Hero from './components/Home-page/Hero/Hero'
 import Events from './components/Home-page/Explore-Events/Events'
-import EventDetails from './components/EventDetails/EventDetails'
 import Footer from './components/Home-page/Footer/Footer'
 import AboutUs from './pages/AboutUs'
 import Cinema from './pages/Cinema'
+import EventDetails from './pages/EventDetails/EventDetails'
 import SignUp from './pages/SignUp'
 
 import './App.css'
@@ -29,6 +29,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/cinema" element={<Cinema />} />
+        <Route path="/cinema/EventDetails" element={<EventDetails />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
