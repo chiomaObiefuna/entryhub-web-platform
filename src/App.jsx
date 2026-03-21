e2ae86d77af3972ab0be5dd30793d478ba3e2f88
Jolayemi Boluwatife <bolujolayemi@gmail.com>
2026-03-21 15:47:31 +0100

    refactor: update routes to serve EventPage at /cinema

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,12 @@ import Events from './components/Home-page/Explore-Events/Events'
 import EventDetails from './components/Home-page/Event-Details/EventDetails'
 import Footer from './components/Home-page/Footer/Footer'
 import AboutUs from './pages/AboutUs'
-import Cinema from './pages/Cinema'
+// import Cinema from './pages/Cinema' // You can comment this out if you're replacing it
 import SignUp from './pages/SignUp'
 import EventPage from './EventPage' 
 
 import './App.css'
 
-// We group Hero, Events, and Details for the main landing page
 function Home() {
   return (
     <>
@@ -31,12 +30,11 @@ function App() {
         <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
         <Route path="/home" element={<><Navbar /><Home /><Footer /></>} />
         <Route path="/about-us" element={<><Navbar /><AboutUs /><Footer /></>} />
-        <Route path="/cinema" element={<><Navbar /><Cinema /><Footer /></>} /> 
         <Route path="/sign-up" element={<><Navbar /><SignUp /><Footer /></>} />
 
-        {/* 2. YOUR DASHBOARD (NO NAVBAR/FOOTER) */}
-        {/* This allows your Sidebar to take up the whole left side */}
-        <Route path="/event" element={<EventPage />} />
+        {/* 2. DASHBOARD PAGE (Standalone layout) */}
+        {/* We changed the path from /event to /cinema here */}
+        <Route path="/cinema" element={<EventPage />} />
 
         {/* CATCH-ALL */}
         <Route path="*" element={<Navigate to="/" replace />} />
