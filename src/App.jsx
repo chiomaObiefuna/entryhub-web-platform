0c157bf8af82908e963ce298ef44dc2cdd071f3d
Chioma Obiefuna <chioma.obiefuna7@gmail.com>
2026-03-23 21:38:35 +0100

    fix: force update file casing for vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ import TicketResales from './components/Ticket/TicketResales'
 import DashboardLayout from './components/Dashboard-Layout/DashboardLayout';
 
 import './App.css';
-import BookEvents from './components/Book-Event/BookEvents';
+
 import DetailsEvents from './components/Details-Events/DetailsEvents';
+
+import BookEvents from './components/Book-Event/BookEvents.JSX';
 import PaymentComplete from './components/Payment-Complete/PaymentComplete';
 import Bankdetails from './components/Home-page/Payment-method/bank-details'
 
